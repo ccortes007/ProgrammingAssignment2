0bc6a3962812d1b69aaa41b24ad7b4ca8f09d26a
ccortes007 <ccortes007@hotmail.com>
2014-12-19 22:28:50 -0800

    R Programming Assignment 2
    Adding unit testing examples

--- cachematrix.R
+++ cachematrix.R
@@ -3,7 +3,8 @@
 ## The following functions create a global object that stores a numeric matrix and caches its inverse
 
 ## Write a short comment describing this function
-# The makeCacheMatrix creates a special "matrix", which is really a list containing a function to
+# The makeCacheMatrix creates a special "matrix", which is really a list 
+# containing a function to
 # 1. Set the value of the matrix
 # 2. Get the value of the matrix
 # 3. Set the value of the inverse matrix
@@ -24,9 +25,12 @@ makeCacheMatrix <- function(x = matrix()) {
 
 
 ## Write a short comment describing this function
-## cacheSolve function calculates the inverse matrix of the special "matrix" created with the makeCacheMatriz
-## cacheSolve first checks if inverse matrix has already been calculated and if so return it and skip calculation.
-## Otherwise, it calculates the inverse matrix of the data and stored it via the setinverse function.
+## cacheSolve function calculates the inverse matrix of the special "matrix" 
+## created with the makeCacheMatrix
+## cacheSolve first checks if inverse matrix has already been calculated and 
+## if so return it and skip calculation.
+## Otherwise, it calculates the inverse matrix of the data and stored it via 
+##the setinverse function.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
@@ -40,3 +44,30 @@ cacheSolve <- function(x, ...) {
         x$setinverse(m)       # set the global m with the value of local m
         m                     # Display local m with the inverse matrix of the argument matrix
 }
+
+
+# Unit testing 
+
+# onematrix = makeCacheMatrix(matrix(c(1,2,3,4), nrow=2, ncol=2))
+# onematrix$get()
+# onematrix$getinverse()
+# cacheSolve(onematrix)
+# cacheSolve(onematrix)
+
+# onematrix = makeCacheMatrix(matrix(c(1,2,4,7,6,8,5,3,9), nrow=3, ncol=3))
+# onematrix$get()
+# onematrix$getinverse()
+# cacheSolve(onematrix)
+# cacheSolve(onematrix)
+
+# onematrix = makeCacheMatrix(matrix(c(1,1,1,1,1,2,3,4,1,3,6,10,1,4,10,20), nrow=4, ncol=4))
+# onematrix$get()
+# onematrix$getinverse()
+# cacheSolve(onematrix)
+# cacheSolve(onematrix)
+
+# onematrix = makeCacheMatrix(matrix(c(-1, -2, 1, 1), nrow=2, ncol=2))
+# onematrix$get()
+# onematrix$getinverse()
+# cacheSolve(onematrix)
+# cacheSolve(onematrix)
